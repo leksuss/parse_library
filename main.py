d1ffae7a3175f15ff5ec4b9dac58f34becc98cc8
Andrey Alekseev <leksus@leksus.info>
2023-04-11 15:22:48 +0300

    refactor: split code into functions

--- main.py
+++ main.py
@@ -4,14 +4,23 @@ import requests
 
 FOLDER_NAME = 'books'
 
-os.makedirs(FOLDER_NAME, exist_ok=True)
 
-for book_id in range(1, 11):
+def download_book(book_id):
     url = f'https://tululu.org/txt.php?id={book_id}'
-    response = requests.get(url)
+    response = requests.get(url, allow_redirects=False)
     response.raise_for_status()
 
-    filename = f'{book_id}.txt'
-    filepath = os.path.join(FOLDER_NAME, filename)
-    with open(filepath, 'wb') as file:
-        file.write(response.content)
+    if response.content:
+        filename = f'{book_id}.txt'
+        filepath = os.path.join(FOLDER_NAME, filename)
+        with open(filepath, 'wb') as file:
+            file.write(response.content)
+    else:
+        return
+
+
+if __name__ == '__main__':
+    os.makedirs(FOLDER_NAME, exist_ok=True)
+
+    for book_id in range(1, 11):
+        download_book(book_id)
