99b386f1cedcca56645cd5d75ac1fb90d3c29efc
Andrey Alekseev <leksus@leksus.info>
2023-05-11 23:31:46 +0300

    split books by two columns

--- render_website.py
+++ render_website.py
@@ -1,8 +1,8 @@
 import json
-from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-from livereload import Server, shell
+from livereload import Server
+from more_itertools import chunked
 
 import parse_tululu_category
 
@@ -13,7 +13,7 @@ PAGE_NAME = 'index'
 def render_page(html_template, page, context, env):
     template = env.get_template(html_template)
     rendered_content = template.render(**context)
-    with open(page, 'w', encoding="utf8") as file:
+    with open(f'{page}.html', 'w', encoding="utf8") as file:
         file.write(rendered_content)
 
 
@@ -27,7 +27,7 @@ def main():
     with open(parse_tululu_category.BOOKS_FILENAME, 'r') as f:
         books = json.load(f)
 
-    render_page(TEMPLATE, PAGE_NAME, {'books': books}, env)
+    render_page(TEMPLATE, PAGE_NAME, {'books': chunked(books, 2)}, env)
     server = Server()
     server.watch(
         TEMPLATE, 
