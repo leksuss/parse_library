da1f9a9cd77065cf5fabded375b2b3cd9ef68a0a
Andrey Alekseev <leksus@leksus.info>
2023-05-12 16:12:04 +0300

    split books list by pages

--- render_website.py
+++ render_website.py
@@ -1,4 +1,5 @@
 import json
+import  os
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
@@ -8,16 +9,20 @@ import parse_tululu_category
 
 TEMPLATE = 'template.html'
 PAGE_NAME = 'index'
+DIR_NAME = 'pages'
 
 
-def render_page(html_template, page, context, env):
+def render_page(html_template, page_name, books, dir_name, env):
     template = env.get_template(html_template)
-    rendered_content = template.render(**context)
-    with open(f'{page}.html', 'w', encoding="utf8") as file:
-        file.write(rendered_content)
+    for i, books_chunk in enumerate(chunked(books, 20), 1):
+        rendered_content = template.render({'books': chunked(books_chunk, 2)})
+        filename = os.path.join(dir_name, f'{page_name}{i}.html')
+        with open(filename, 'w', encoding="utf8") as file:
+            file.write(rendered_content)
 
 
 def main():
+    os.makedirs(DIR_NAME, exist_ok=True)
 
     env = Environment(
         loader=FileSystemLoader('.'),
@@ -27,15 +32,14 @@ def main():
     with open(parse_tululu_category.BOOKS_FILENAME, 'r') as f:
         books = json.load(f)
 
-    render_page(TEMPLATE, PAGE_NAME, {'books': chunked(books, 2)}, env)
+    render_page(TEMPLATE, PAGE_NAME, books, DIR_NAME, env)
     server = Server()
     server.watch(
         TEMPLATE, 
         lambda: render_page(
-            TEMPLATE, PAGE_NAME, {'books': chunked(books, 2)}, env
+            TEMPLATE, PAGE_NAME, {'books': books}, DIR_NAME, env
         )
     )
-
     server.serve(root='.')
 
 if __name__ == '__main__':
