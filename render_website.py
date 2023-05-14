8dc42ea0f26d7ee023f56431503d9fd48a6ad3bf
Andrey Alekseev <leksus@leksus.info>
2023-05-14 12:57:33 +0300

    prepared for work site in separate folder

--- render_website.py
+++ render_website.py
@@ -8,45 +8,48 @@ from more_itertools import chunked
 import parse_tululu_category
 
 TEMPLATE = 'template.html'
-PAGE_NAME = 'index'
-DIR_NAME = 'pages'
+WEBSITE_DIR = 'website'
+PAGES_SUBDIR = 'pages'
 
 
-def render_page(html_template, page_name, books, dir_name, env):
+def render_page(html_template, books, pages_dir, env):
     template = env.get_template(html_template)
     chunked_books = list(chunked(books, 20))
     pages = range(1, len(chunked_books) + 1)
+
     for page_id, books_chunk in enumerate(chunked_books, 1):
         rendered_content = template.render({
-            'books': chunked(books_chunk, 2),
+            'books': list(chunked(books_chunk, 2)),
             'pages': pages,
             'current_page': page_id,
         })
-        filename = os.path.join(dir_name, f'{page_name}{page_id}.html')
+        filename = os.path.join(pages_dir, f'index{page_id}.html')
         with open(filename, 'w', encoding="utf8") as file:
             file.write(rendered_content)
 
 
 def main():
-    os.makedirs(DIR_NAME, exist_ok=True)
+    pages_dir = os.path.join(WEBSITE_DIR, PAGES_SUBDIR)
+    os.makedirs(pages_dir, exist_ok=True)
 
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
 
-    with open(parse_tululu_category.BOOKS_FILENAME, 'r') as f:
+    books_filepath = os.path.join(
+        WEBSITE_DIR, parse_tululu_category.BOOKS_FILENAME
+    )
+    with open(books_filepath, 'r') as f:
         books = json.load(f)
 
-    render_page(TEMPLATE, PAGE_NAME, books, DIR_NAME, env)
+    render_page(TEMPLATE, books, pages_dir, env)
     server = Server()
     server.watch(
         TEMPLATE, 
-        lambda: render_page(
-            TEMPLATE, PAGE_NAME, {'books': books}, DIR_NAME, env
-        )
+        lambda: render_page(TEMPLATE, books, pages_dir, env),
     )
-    server.serve(root='.')
+    server.serve(root=WEBSITE_DIR)
 
 if __name__ == '__main__':
     main()
